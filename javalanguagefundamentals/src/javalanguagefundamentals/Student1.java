c3ffcee8f6d2a60b4fbb59cfeca9da5194d4cc80
nikhi0329 <karandekarnikhitha@gmail.com>
2026-09-21 16:03:57 +0530

    Student1.java

---
+++ javalanguagefundamentals/src/javalanguagefundamentals/Student1.java
@@ -0,0 +1,19 @@
+package javalanguagefundamentals;
+
+public class Student1 {
+ int id;
+ String name;
+ int marks;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        Student1 s1=new Student1();
+        s1.id=20;
+        s1.name="chinnuga";
+        s1.marks=100;
+        System.out.println("Student ID is:"+s1.id);
+        System.out.println("Student Name is:"+s1.name);
+        System.out.println("Student Marks is:"+s1.marks);
+	}
+
+}
