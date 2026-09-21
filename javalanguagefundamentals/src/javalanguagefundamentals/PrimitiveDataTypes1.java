c6a515f76a4064ed89826db8f9e0b170cc551e92
nikhi0329 <karandekarnikhitha@gmail.com>
2026-09-21 15:38:48 +0530

    PrimitiveDataTypes1.java

---
+++ javalanguagefundamentals/src/javalanguagefundamentals/PrimitiveDataTypes1.java
@@ -0,0 +1,25 @@
+package javalanguagefundamentals;
+
+public class PrimitiveDataTypes1 {
+       byte b=34;
+       short s=4567;
+       int n=456783;
+       long l=678453L;
+       float f=5987.76f;
+       double d=46785763.875;
+       char c='M';
+       boolean b1=true;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrimitiveDataTypes1 t1=new PrimitiveDataTypes1();
+        System.out.println("byte Value is:"+t1.b);
+        System.out.println("Short value is:"+t1.s);
+        System.out.println("Int values is:"+t1.n);
+        System.out.println("Long value is:"+t1.l);
+        System.out.println("Float values is:"+t1.f);
+        System.out.println("Double value is:"+t1.d);
+        System.out.println("Char value is:"+t1.c);
+        System.out.println("boolean value is:"+t1.b1);
+	}
+
+}
