659163c5bf537ad045d404104557ed3106470fcf
nikhi0329 <karandekarnikhitha@gmail.com>
2026-09-21 16:24:56 +0530

    Add Student2.java

---
+++ javalanguagefundamentals/src/javalanguagefundamentals/Student2.java
@@ -0,0 +1,17 @@
+package javalanguagefundamentals;
+
+public class Student2 {
+      
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=10;
+      Integer i1=100;
+      Integer a1;
+      System.out.println(a1);
+      double result=a+i1;
+     int d=(int)result;
+     System.out.println(result);
+      System.out.println(d);
+	}
+
+}
